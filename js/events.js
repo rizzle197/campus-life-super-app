5a9ab7831628eedccc12b3e255ce405c741aebf3
rizzle197 <connorgibson986@gmail.com>
2025-12-05 04:03:08 -0500

    added ticketmaster functionality for use in events.html

--- js/events.js
+++ js/events.js
@@ -10,20 +10,38 @@ const eventsContainer = document.querySelector("#events-container");
 const fetchButton = document.querySelector("#fetch-events-btn");
 
 // API URL
-const apiURL = ""
+const apiURL = "https://app.ticketmaster.com/discovery/v2/events.json?city=Atlanta&size=3&apikey=KtLuGRAbY9IaaQORYWXMXx3RwP6DuIMx";
 
 // When user clicks the button, it will run this
 fetchButton.addEventListener("click", async function() {
-    const response = await fetch(apiURL);
-    const data = await response.json();
-
-    // This will loop through events in response and display the first 3 events
-    
-    // Still working on this part
-
-    // For loop here
+    console.log("Loading Events.");
 
     // Clearing it every time you click the button so it doesn't stack the events every time you click
     eventsContainer.innerHTML = "";
 
+    // Fetching the events from Ticketmaster
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    // Checking to make sure that there are events 
+    const events = data._embedded.events;
+    
+    // Loop to get info about each event and display it on the webpage
+    for (let i = 0; i < events.length; i++) {
+        // Getting info
+        const event = events[i];
+        const venue = event._embedded.venues[0];
+
+        // HTML that will appear on the webpage for each event
+        const eventHTML = `
+            <div class="yellow-text-box">
+                <h3>${event.name}</h3>
+                <p>Date: ${event.dates.start.localDate} ${event.dates.start.localTime}</p>
+                <p>Venue: ${venue.name}</p>
+                <p>Location: ${venue.city.name}, ${venue.state.stateCode}</p>
+                <a href="${event.url}">View Tickets</a>
+            </div>
+        `;
+        eventsContainer.innerHTML += eventHTML;
+    }
 });
